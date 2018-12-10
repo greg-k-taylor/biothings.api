3801babee24019d16d3d304e6d124c1ce835effd
Greg Taylor <greg.k.taylor@gmail.com>
2018-12-10 14:16:22 -0800

    datatransform/histogram.py - track conversion rate for each edge, fixup

--- biothings/hub/datatransform/datatransform_mdb.py
+++ biothings/hub/datatransform/datatransform_mdb.py
@@ -289,7 +289,7 @@ class DataTransformMDB(DataTransform):
         # Return a list of documents that have had their identifiers replaced
         # also return a list of documents that were not changed
         hit_lst, miss_lst = _build_hit_miss_lsts(doc_lst, saved_hits)
-        self.histogram.update_io(input_type,target,len(hit_lst), len(his_lst) + len(miss_lst))
+        self.histogram.update_io(input_type,target,len(hit_lst), len(hit_lst) + len(miss_lst))
         return hit_lst, miss_lst
 
     def _edge_lookup(self, edge_obj, id_strct):
